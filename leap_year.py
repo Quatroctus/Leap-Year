b7e921d44de4d3fb851daae0fdb94e2ff24cc96c
Anime-Quatroctus <kanadetachibona@gmail.com>
2021-04-24 20:52:49 -0700

    Added function docstrings.

--- leap_year.py
+++ leap_year.py
@@ -2,10 +2,15 @@ from typing import Callable
 
 
 def leap_year(year: int) -> bool:
+    """Return whether the int parameter is a leap year.
+    If the year is a multiple of 4 and the year is not a multiple of 100.
+    Or year is a multiple of 400.
+    """
     return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
 
 def validate_int(s: str) -> bool:
+    """Return True if the string can be converted to an integer using base 10 digits, otherwise return False."""
     try:
         int(s)
         return True
@@ -15,6 +20,7 @@ def validate_int(s: str) -> bool:
 
 
 def validate_year(s: str) -> bool:
+    """Return True if the string when converted to an integer using base 10 digits is a positive integer, otherwise return False."""
     if int(s) > 0:
         return True
     print("Years cannot be negative or zero.")
@@ -22,6 +28,7 @@ def validate_year(s: str) -> bool:
 
 
 def int_input(message: str, validate: Callable[[str], bool] = validate_int) -> int:
+    """Obtain user input from the console until validate returns True and return that input as an int."""
     answer = input(message)
     while not validate(answer):
         answer = input(message)
