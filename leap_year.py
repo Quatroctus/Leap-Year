df359e21b25b3e4095b10c47cfdf7bb6793f9b60
Anime-Quatroctus <kanadetachibona@gmail.com>
2021-04-24 20:26:35 -0700

    Added basic string to int user input validation.

--- leap_year.py
+++ leap_year.py
@@ -1,4 +1,14 @@
 def leap_year(year: int) -> bool:
     return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
-print(leap_year(int(input("Enter a year: "))))
+def int_input(message: str):
+    while True:
+        try:
+            answer = input(message)
+            i = int(answer)
+            return i
+        except ValueError:
+            print("Input is not an integer.")
+    raise RuntimeError()
+
+print(leap_year(int_input("Enter a year: ")))
